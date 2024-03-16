309bc81e458b967c5644e3c1d36bf041968f0ca9
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2024-03-16 14:01:38 +0100

    Add function that lets the player choose an option and validate it

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //function that allows the computer to generate a random choice in the game (rock, paper or scissors)
 function getComputerChoice() {
-    computerChoice = Math.floor(Math.random() * 3 + 1);
-    switch (computerChoice) {
+    computerSelection = Math.floor(Math.random() * 3 + 1);
+    switch (computerSelection) {
         case 1:
             return 'Rock';
         case 2:
@@ -12,4 +12,37 @@ function getComputerChoice() {
 
 }
 
+//function to make sure the first letter is upper case and the rest is lower case
+function changeCase(choice) {
+    choice.toLowerCase();
+    firstLetter = choice.slice(0, 1);
+    restOfString = choice.slice(1);
+    return firstLetter.toUpperCase() + restOfString.toLowerCase();
+}
+
+//function that checks if the player entered a correct value
+function validatePlayerChoice(playerSelection) {
+    if (playerSelection == 'Rock' || playerSelection == 'Paper' || playerSelection == 'Scissors') {
+        validChoice = true;
+    }
+    else {
+        validChoice = false;
+    }
+    return validChoice;
+}
+
+//function that plays a single round of Rock, Paper, Scissors
+function playGame(playerSelection, computerSelection) {
+    playerSelection = prompt("Please choose rock, paper or scissors: ");
+    playerSelection = changeCase(playerSelection);
+    validChoice = validatePlayerChoice(playerSelection);
+    while (validChoice != true) {
+        playerSelection = prompt("The value is incorrect.\nPlease enter 'Rock', 'Paper' or 'Scissors': ");
+        playerSelection = changeCase(playerSelection);
+        validChoice = validatePlayerChoice;
+    }
+    console.log(playerSelection);
+}
+
 console.log(getComputerChoice());
+playGame();
