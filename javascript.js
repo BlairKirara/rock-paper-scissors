989cad746a735b5cc0d3c155d2a58e942d143716
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2024-03-16 13:40:26 +0100

    Make computer choose Rock, Paper or Scissors

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
 //function that allows the computer to generate a random choice in the game (rock, paper or scissors)
 function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3 + 1);
+    switch (computerChoice) {
+        case 1:
+            return 'Rock';
+        case 2:
+            return 'Paper';
+        case 3:
+            return 'Scissors';
+    }
 
 }
+
+console.log(getComputerChoice());
